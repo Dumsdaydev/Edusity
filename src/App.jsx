bf9daca1218b800dac9fcf43613953d6ea4f039b
Dumsdaydev <dumsdayconcept@gmail.com>
2025-09-16 15:31:11 -0700

    Fix case sensitivity for imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import Navbar from './components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import Programs from './Components/Programs/Programs'
-import Title from './Components/Title/Title'
-import About from './Components/About/About'
-import Campus from './Components/Campus/Campus'
-import Testimonials from './Components/Testimonials/Testimonials'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
-import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import Programs from './Components/Programs/Programs';
+import Title from './Components/Title/Title';
+import About from './Components/About/About';
+import Campus from './Components/Campus/Campus';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer';
 
 
 
